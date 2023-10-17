d33fe9618a7af3b02a84220b91e2dff9f92b1460
MirkoGLostia <giggi.torre@mail.com>
2023-10-17 14:03:12 +0200

    hello java message

--- src/org/lessons/java/StampaSaluto.java
+++ src/org/lessons/java/StampaSaluto.java
@@ -1,2 +1,9 @@
-package org.lessons.java;public class StampaSaluto {
+package org.lessons.java;
+
+import org.w3c.dom.ls.LSOutput;
+
+public class StampaSaluto {
+    public static void main(String[] args) {
+        System.out.println("Hello Java");
+    }
 }
